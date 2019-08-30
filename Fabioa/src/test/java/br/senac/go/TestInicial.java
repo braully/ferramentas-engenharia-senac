31e7f25e8e7efd3d1d3ce15ddaa59343868597f4
Aluno <Aluno@PC-43641>
2019-08-30 16:45:47 -0300

    Correçao do modulo

---
+++ Fabioa/src/test/java/br/senac/go/TestInicial.java
@@ -0,0 +1,12 @@
+package br.senac.go;
+
+import org.junit.Test;
+
+public class TestInicial {
+
+    @Test
+    public void testePrevio() throws Exception {
+        Class.forName("br.senac.go.HelloWord");
+    }
+
+}
