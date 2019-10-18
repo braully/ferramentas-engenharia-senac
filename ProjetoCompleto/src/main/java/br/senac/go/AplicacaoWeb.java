8938965cb88dc966c15f07570a80d3cdee351fe6
Braully Rocha <braully@gmail.com>
2019-10-18 19:13:06 -0300

    correção para tomcat

--- ProjetoCompleto/src/main/java/br/senac/go/AplicacaoWeb.java
+++ ProjetoCompleto/src/main/java/br/senac/go/AplicacaoWeb.java
@@ -2,13 +2,14 @@ package br.senac.go;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  *
  * @author strike
  */
 @SpringBootApplication
-public class AplicacaoWeb {
+public class AplicacaoWeb extends SpringBootServletInitializer {
 
     public static void main(String... args) {
         SpringApplication.run(AplicacaoWeb.class, args);
