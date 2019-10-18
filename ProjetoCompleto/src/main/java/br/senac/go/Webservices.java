d7b79ea85847ea640f0e2978bfab8da9cbd69442
Braully Rocha <braully@gmail.com>
2019-10-18 08:53:13 -0300

    pagina de braully.

--- ProjetoCompleto/src/main/java/br/senac/go/Webservices.java
+++ ProjetoCompleto/src/main/java/br/senac/go/Webservices.java
@@ -1,5 +1,8 @@
 package br.senac.go;
 
+import static j2html.TagCreator.*;
+import j2html.tags.ContainerTag;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -8,6 +11,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Webservices {
+
+    static String[] MODULOS = {
+        "Adriel", "Anibal",
+        "Brendon", "Fabioa",
+        "Fabioc", "Iury",
+        "Izaias", "Joaoc",
+        "Joaor", "Maidno",
+        "Marcelo", "Nicholas",
+        "Rafaelc", "Rafaelg",
+        "Wilker", "Yuri", "Braully"
+    };
+
+    @GetMapping("/modulos")
+    public String modulos() {
+        ContainerTag html = html();
+        ContainerTag body = body();
+        ContainerTag ul = ul();
+        body.with(ul);
+        Arrays.stream(MODULOS).forEach(m -> ul.with(li(a(m).withHref(m + ".html"))));
+        return html.with(body).renderFormatted();
+    }
 
     @GetMapping("/hello")
     public Collection<String> sayHello() {
